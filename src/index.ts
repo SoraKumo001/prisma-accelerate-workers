746ca2a787fb031129b4d1d27de9451e3d65ede8
SoraKumo <info@croud.jp>
2024-02-04 11:38:52 +0900

    Support for cloudflare

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,136 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { PrismaPg } from '@prisma/adapter-pg';
+import WASM from '@prisma/client/runtime/query-engine.wasm';
+import { PrismaAccelerate, PrismaAccelerateConfig, ResultError } from 'prisma-accelerate-local/lib';
+import { getPrismaClient } from '@prisma/client/runtime/wasm.js';
+import pg from 'pg';
 
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
+	SECRET: string;
+	KV: KVNamespace;
 }
+
+const getAdapter = (datasourceUrl: string) => {
+	const url = new URL(datasourceUrl);
+	const schema = url.searchParams.get('schema');
+	const pool = new pg.Pool({
+		connectionString: url.toString(),
+	});
+	return new PrismaPg(pool, {
+		schema: schema ?? undefined,
+	});
+};
+
+let prismaAccelerate: PrismaAccelerate;
+const getPrismaAccelerate = async ({
+	secret,
+	onRequestSchema,
+	onChangeSchema,
+}: {
+	secret: string;
+	onRequestSchema: PrismaAccelerateConfig['onRequestSchema'];
+	onChangeSchema: PrismaAccelerateConfig['onChangeSchema'];
+}) => {
+	if (prismaAccelerate) {
+		return prismaAccelerate;
+	}
+	prismaAccelerate = new PrismaAccelerate({
+		secret,
+		adapter: (datasourceUrl) => getAdapter(datasourceUrl),
+		getQueryEngineWasmModule: async () => {
+			return WASM;
+		},
+		getPrismaClient: getPrismaClient as never,
+		onRequestSchema,
+		onChangeSchema,
+	});
+	return prismaAccelerate;
+};
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		const prismaAccelerate = await getPrismaAccelerate({
+			secret: env.SECRET ?? 'test',
+			onRequestSchema: ({ engineVersion, hash, datasourceUrl }) => {
+				return env.KV.get(`${engineVersion}:${hash}:${datasourceUrl}`);
+			},
+			onChangeSchema: ({ inlineSchema, engineVersion, hash, datasourceUrl }) => {
+				return env.KV.put(`${engineVersion}:${hash}:${datasourceUrl}`, inlineSchema, { expirationTtl: 60 * 60 * 24 * 7 });
+			},
+		});
+
+		const url = new URL(request.url);
+		const paths = url.pathname.split('/');
+		const [_, version, hash, command] = paths;
+		const headers = Object.fromEntries(request.headers.entries());
+		const createResponse = (result: Promise<unknown>) =>
+			result
+				.then((r) => {
+					return new Response(JSON.stringify(r), {
+						headers: { 'content-type': 'application/json' },
+					});
+				})
+				.catch((e) => {
+					if (e instanceof ResultError) {
+						return new Response(JSON.stringify(e.value), {
+							status: e.code,
+							headers: { 'content-type': 'application/json' },
+						});
+					}
+					return new Response(JSON.stringify(e), {
+						status: 500,
+						headers: { 'content-type': 'application/json' },
+					});
+				});
+
+		if (request.method === 'POST') {
+			const body = await request.text();
+			switch (command) {
+				case 'graphql':
+					return createResponse(prismaAccelerate.query({ body, hash, headers }));
+				case 'transaction':
+					return createResponse(
+						prismaAccelerate.startTransaction({
+							body,
+							hash,
+							headers,
+							version,
+						})
+					);
+				case 'itx': {
+					const id = paths[4];
+					switch (paths[5]) {
+						case 'commit':
+							return createResponse(
+								prismaAccelerate.commitTransaction({
+									id,
+									hash,
+									headers,
+								})
+							);
+						case 'rollback':
+							return createResponse(
+								prismaAccelerate.rollbackTransaction({
+									id,
+									hash,
+									headers,
+								})
+							);
+					}
+				}
+			}
+		} else if (request.method === 'PUT') {
+			const body = await request.text();
+			switch (command) {
+				case 'schema':
+					return createResponse(
+						prismaAccelerate.updateSchema({
+							body,
+							hash,
+							headers,
+						})
+					);
+			}
+		}
+		return new Response('Not Found', { status: 404 });
 	},
 };
